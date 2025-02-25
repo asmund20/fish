6eaedf75c67b8165674949c00d8f3d58ba385aaa
]smund Olai Sand-Larsen <asmund20@gmail.com>
2025-02-25 22:03:03 +0100

    Move conda away

--- config.fish
+++ config.fish
@@ -18,17 +18,3 @@ if test -d ~/.config/fish/local
         source $file
     end
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/asmund/miniconda3/bin/conda
-    eval /home/asmund/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/asmund/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/asmund/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/asmund/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
