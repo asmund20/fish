6a619f3bd3ac94b1e83bfdc7c584ada99c90601d
]smund Olai Sand-Larsen <asmund20@gmail.com>
2025-03-11 15:53:51 +0100

    Add space on remote prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     if test $is_remote = "true"
-        echo -n (prompt_hostname) "@" (prompt_pwd)
+        echo -n (prompt_hostname) "@" (prompt_pwd) " "
     else
         echo -n (prompt_pwd) " "
     end
