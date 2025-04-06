5d4e931b79dbfb3a4fcec7893056c6a49740088b
asmund20 <asmund.sl@proton.me>
2025-04-06 14:38:55 +0200

    Add some comments to aliases and add git stuff

--- aliases.fish
+++ aliases.fish
@@ -1,2 +1,10 @@
+# Login to ifi machine
 alias ifi="ssh -J aasmuos@login.uio.no aasmuos@login.ifi.uio.no"
+
+# Start python interpreter with numpy imported as np
 alias numpy="python -i -c 'import numpy as np'"
+
+# Git stuff
+alias branch="git checkout -b"
+alias main="git checkout main"
+alias commit="git commit -m"
