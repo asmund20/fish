fc064502ffe0f958584fc25dad91d6defd1e2d3c
asmund20 <asmund.sl@proton.me>
2025-09-26 11:37:01 +0200

    Update git aliases, remove ssh alias that should be in ssh config

--- aliases.fish
+++ aliases.fish
@@ -1,10 +1,12 @@
-# Login to ifi machine
-alias ifi="ssh -J aasmuos@login.uio.no aasmuos@login.ifi.uio.no"
-
 # Start python interpreter with numpy imported as np
 alias numpy="python -i -c 'import numpy as np'"
 
 # Git stuff
-alias branch="git checkout -b"
-alias main="git checkout main"
-alias commit="git commit -m"
+alias gc="git commit"
+alias gco="git checkout"
+alias gb="git branch"
+alias gs="git status"
+alias ga="git add"
+alias gst="git stash"
+
+alias e="nvim"
